2886863cee4c9d66d00c7ae811b6a60ab4a758fd
AnudeepGunukula <anudeepgunukula@gmail.com>
2021-11-09 23:28:52 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using YoutubeDownloader.Models;
+
+namespace YoutubeDownloader.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            url url = new url();
+            return View(url);
+        }
+
+        [HttpPost]
+        public IActionResult Index(url url)
+        {
+            return View("FinalIndex",url);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/url.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YoutubeDownloader.Models
+{
+    public class url
+    {
+        private string youtubeUrl;
+
+        private string format;
+
+        public List<string> FormatList { get; set; }
+        public string YoutubeUrl { get => youtubeUrl; set => youtubeUrl = value; }
+        public string Format { get => format; set => format = value; }
+
+        public url()
+        {
+            FormatList = new List<string>()
+            {
+               "mp3","m4a","aac","flac","opus","ogg","wav","webm","360","480","720p","1080","4k","8k"
+            };
+
+        }
+
+    }
+}
